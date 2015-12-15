1092fe6abf92b17340cef479935ec167bfd1bd60
speed007 <emailz.to@gmail.com>
2015-12-15 16:56:21 +0000

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -6,34 +6,40 @@ public class Player : MonoBehaviour
 	private Rigidbody2D myRigidbody = null; // To cache the rigidboy2D
 	private Vector3 vec3Scale; // To cache the vectors of the object
 	private float flip = -1; // To turn the player to left from right or to flip upside down
-	private float vertical; // To cache the up/down movement  (Jump or Drop)
 	[SerializeField]
 	private float movementSpeed;
 	private bool facingRight;
-
+	private float horizontal;
+//	private float vertical; // To cache the up/down movement  (Jump or Drop)
 	
 	void Awake () 
 	{
 		facingRight = true;
 		myRigidbody = GetComponent<Rigidbody2D> ();
-		vertical = Input.GetAxis("Vertical");
+		//
 	}
 
 	void FixedUpdate () 
 	{
-		float horizontal = Input.GetAxis("Horizontal");
-		HandleMovement (horizontal);
-		Flip (horizontal);
+		Axis ();
+		HandleMovement ();
+		Flip ();
 	}
 
-	 private void HandleMovement(float horizontal)
+	private void Axis()
+	{
+		horizontal = Input.GetAxis("Horizontal");
+//		vertical = Input.GetAxis("Vertical");
+	}
+
+	 private void HandleMovement()
 	{
 		myRigidbody.velocity = new Vector2 (horizontal * movementSpeed, myRigidbody.velocity.y);
 	}
 
-	private void Flip( float horizontal)
+	private void Flip()
 	{
-		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) 
 		{
 			facingRight = !facingRight;
 			Vector3 theScale = transform.localScale;
@@ -41,3 +47,8 @@ public class Player : MonoBehaviour
 			transform.localScale = theScale;
 		}
 	}
+
+
+
+
+}
